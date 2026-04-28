cd586c518c403cdb77e893a344be8fc97a73c6d0
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-04-29 02:01:41 +0800

    fix: add get_player() guard to not access freed player

--- entities/enemies/trojan/trojan.gd
+++ entities/enemies/trojan/trojan.gd
@@ -6,13 +6,16 @@ const SPEED = 200
 @onready var dash_timer: Timer = $DashTimer
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-@onready var floppy_disk: Node = get_tree().get_nodes_in_group("player")[0]
+@onready var floppy_disk: Node = _get_player()
 var direction : Vector2
 var is_dashing := false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if floppy_disk == null:
+	if not is_instance_valid(floppy_disk):
+		floppy_disk = _get_player()
+	
+	if not is_instance_valid(floppy_disk):
 		return
 
 	if not is_dashing:
@@ -21,7 +24,10 @@ func _process(delta: float) -> void:
 	position += direction * SPEED * delta
 
 func _on_cooldown_timer_timeout() -> void:
-	if floppy_disk == null:
+	if not is_instance_valid(floppy_disk):
+		floppy_disk = _get_player()
+	
+	if not is_instance_valid(floppy_disk):
 		return
 		
 	direction = global_position.direction_to(floppy_disk.global_position)
@@ -38,3 +44,10 @@ func _on_dash_timer_timeout() -> void:
 	sprite.play("idle")
 	is_dashing = false
 	cooldown_timer.start()
+
+func _get_player() -> Node:
+	var players := get_tree().get_nodes_in_group("player")
+	if players.is_empty():
+		return null
+	
+	return players[0]
--- entities/enemies/virus/virus.gd
+++ entities/enemies/virus/virus.gd
@@ -2,10 +2,21 @@ extends Node2D
 
 const SPEED = 75
 
-@onready var floppy_disk: Node = get_tree().get_nodes_in_group("player")[0]
+@onready var floppy_disk: Node = _get_player()
 
 func _process(delta: float) -> void:
-	if floppy_disk == null:
+	if not is_instance_valid(floppy_disk):
+		floppy_disk = _get_player()
+	
+	if not is_instance_valid(floppy_disk):
 		return
+	
 	var direction = global_position.direction_to(floppy_disk.global_position)
 	position += direction * SPEED * delta
+
+func _get_player() -> Node:
+	var players := get_tree().get_nodes_in_group("player")
+	if players.is_empty():
+		return null
+	
+	return players[0]
